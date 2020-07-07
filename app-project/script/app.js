dd59a3f1e17ed8bead85afd7762e12fc3556a14c
Saulo Narciso <saulo.narciso@gmail.com>
2020-07-07 17:06:00 -0300

    commint

--- app-project/script/app.js
+++
@@ -1,3 +0,0 @@
-$("#green").slideUp(2000).slideDown(2000).slideUp(2000).slideDown(2000).slideUp(2000).slideDown(2000).slideUp(2000).slideDown(2000);
-$("#red").slideUp(1000).slideDown(1000).slideUp(1000).slideDown(1000).slideUp(1000).slideDown(1000).slideUp(1000).slideDown(1000);
-$("#blue").slideUp(1500).slideDown(1500).slideUp(1500).slideDown(1500).slideUp(1500).slideDown(1500).slideUp(1500).slideDown(1500);
